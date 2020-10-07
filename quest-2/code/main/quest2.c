8baf4be7c1467568181db9b3a1944f255063a753
Hussain Valiuddin <60791769+hussainv5253@users.noreply.github.com>
2020-10-07 17:16:59 -0400

    Update quest2.c

--- quest-2/code/main/quest2.c
+++ quest-2/code/main/quest2.c
@@ -1,68 +1,53 @@
 //Quest 2
 //Mario Han, Vivek Cherian, Hussain Valiuddin
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include "driver/i2c.h"
+#include "driver/uart.h"
+#include "esp_vfs_dev.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "freertos/queue.h"
+#include "driver/periph_ctrl.h"
+#include "driver/timer.h"
+#include "esp_types.h"
+#include "esp_attr.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
-#include "driver/i2c.h"
-#include "sdkconfig.h"
 #include "esp_vfs_dev.h"
 #include "driver/uart.h"
 #include "math.h"
 
 #define DEFAULT_VREF 1100 //Use adc2_vref_to_gpio() to obtain a better estimate
-#define NO_OF_SAMPLES 20  //Multisampling
+#define NO_OF_SAMPLES 10  //Multisampling
 
 static esp_adc_cal_characteristics_t *adc_chars;
-static const adc_channel_t channel1 = ADC_CHANNEL_0; //Thermistor GPIO 36
-static const adc_channel_t channel2 = ADC_CHANNEL_3; //IR GPIO 39
-static const adc_channel_t channel3 = ADC_CHANNEL_6; //ultrasonic GPIO 34
+static const adc_channel_t channel1 = ADC_CHANNEL_3; //Thermistor GPIO 36 A3
+static const adc_channel_t channel2 = ADC_CHANNEL_6; //IR GPIO 39 A2
+static const adc_channel_t channel3 = ADC_CHANNEL_0; //ultrasonic GPIO 34 A4
 static const adc_atten_t atten = ADC_ATTEN_DB_11;
 static const adc_unit_t unit = ADC_UNIT_1;
 
 static double temperature;
-static double ultrasonic;
-static double ir_rangefinder;
+static int ultrasonic;
+static int ir_rangefinder;
 
-void init()
-{
-    // ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0,
-    //                                     256, 0, 0, NULL, 0));
+// void init()
+// {
+//     //Configure ADC
+//     adc1_config_width(ADC_WIDTH_BIT_12);
+//     adc1_config_channel_atten(channel1, atten);
+//     adc1_config_channel_atten(channel2, atten);
+//     adc1_config_channel_atten(channel3, atten);
 
-    // /* Tell VFS to use UART driver */
-    // esp_vfs_dev_uart_use_driver(UART_NUM_0);
-
-    //Configure ADC
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(channel1, atten);
-    adc1_config_channel_atten(channel2, atten);
-    adc1_config_channel_atten(channel3, atten);
-
-    //Characterize ADC
-    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
-}
-
-// converts voltage to distances in cm
-static uint32_t ultrasonic_voltage_to_distance(uint32_t reading)
-{
-    if (reading == 0)
-    {
-        printf("returning 0");
-        return 0;
-    }
-    else
-    {
-        // uint32_t dist = (1 / .2519685 * (reading))*pow(10,-1); // .2519685 mV per mm
-        uint32_t dist = ((1 / 6.4 * (reading)) - 5) * 2.54; // 6.4 mV per in
-        return dist;
-    }
-}
+//     //Characterize ADC
+//     adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+//     esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+// }
 
 static void thermistor()
 { // push button
@@ -80,14 +65,14 @@ static void thermistor()
         }
         adc_reading /= NO_OF_SAMPLES;
         //Convert adc_reading to voltage in mV
-        //uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
         float temp, rtherm;
         rtherm = adc_reading;
-        rtherm = (5700 * ((4096 / rtherm) - 1));
+        rtherm = (1000 * ((4096 / rtherm) - 1));
         temp = rtherm / 10000;
         temp = log(temp);
         temp /= 3435;
-        temp += 1.0 / (15 + 273.15);
+        temp += 1.0 / (20 + 273.15);
         temp = 1.0 / temp;
         temp -= 273.15;
         temperature = temp;
@@ -106,6 +91,7 @@ static void thermistor()
 
 static void IR_Range()
 { // push button
+
     while (1)
     {
         uint32_t adc_reading = 0;
@@ -140,7 +126,7 @@ static void printstate()
 {
     while (1)
     {
-        printf("Temp:%f, Dist1:%f, Dist2:%f\n", temperature, ultrasonic, ir_rangefinder);
+        printf("Temp:%.1f, Dist1:%d, Dist2:%d\n", temperature, ultrasonic, ir_rangefinder);
         vTaskDelay(2000 / portTICK_RATE_MS);
     }
 }
@@ -159,25 +145,35 @@ static void ultra_sonic()
                 adc_reading += adc1_get_raw((adc1_channel_t)channel3);
                 vTaskDelay(100 / portTICK_RATE_MS);
             }
-
-            adc_reading /= NO_OF_SAMPLES;
-            //Convert adc_reading to voltage in mV
-            uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
-
-            // display voltage
-            uint32_t distance2 = ultrasonic_voltage_to_distance(voltage);
-            ultrasonic = distance2;
-            //printf("Raw: %d\tVoltage: %dmV\tDistance: %din\n", adc_reading, voltage, distance);
         }
+        adc_reading /= NO_OF_SAMPLES;
+        //Convert adc_reading to voltage in mV
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+
+        // display voltage
+        uint32_t dist = ((1 / 6.4 * (voltage)) - 5) * 2.54;
+        // uint32_t distance2 = ultrasonic_voltage_to_distance(voltage);
+        ultrasonic = dist;
+        //printf("Raw: %d\tVoltage: %dmV\tDistance: %din\n", adc_reading, voltage, dist);
     }
 }
 
 void app_main()
 {
-    init(); // Initialize stuff
+    //init(); // Initialize stuff
 
-    xTaskCreate(thermistor, "thermistor", 512, NULL, 5, NULL);
-    xTaskCreate(IR_Range, "IR_Range", 512, NULL, 6, NULL);
-    xTaskCreate(printstate, "printstate", 512, NULL, 8, NULL);
-    xTaskCreate(ultra_sonic, "ultra_sonic", 512, NULL, 7, NULL);
+    //Configure ADC
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(channel1, atten);
+    adc1_config_channel_atten(channel2, atten);
+    adc1_config_channel_atten(channel3, atten);
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+
+    xTaskCreate(thermistor, "thermistor", 4096, NULL, 5, NULL);
+    xTaskCreate(IR_Range, "IR_Range", 4096, NULL, 6, NULL);
+    xTaskCreate(printstate, "printstate", 4096, NULL, 8, NULL);
+    xTaskCreate(ultra_sonic, "ultra_sonic", 4096, NULL, 7, NULL);
 }
