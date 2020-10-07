3a4b2841bedb07f3a8f142bee98fa5c4a0b258a8
v-cherian <vcherian@bu.edu>
2020-10-07 19:08:49 -0400

    successfully running index.js

--- quest-2/code/modules/read_console.py
+++ quest-2/code/modules/read_console.py
@@ -4,12 +4,12 @@ import os
 def myrun(cmd):
     """from http://blog.kagesenshi.org/2008/02/teeing-python-subprocesspopen-output.html
     """
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines = True)
     stdout = []
     while True:
         line = p.stdout.readline()
         print(line)
-        with open('../data/sensors.txt','a') as file:
+        with open('../data/sensors.csv','a') as file:
             file.write(line)
         stdout.append(line)
 
@@ -20,5 +20,5 @@ def myrun(cmd):
 if __name__ == "__main__":
     if not os.path.exists('../data'):
         os.makedirs('../data')
-    os.system("rm ../data/sensors.txt")
+    os.system("rm ../data/sensors.csv")
     myrun("node demo.js")
