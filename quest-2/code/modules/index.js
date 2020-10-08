377ce2ea2cbfcbd21f587eb8dbd6f4104a398d79
v-cherian <vcherian@bu.edu>
2020-10-08 14:56:17 -0400

    pushing code as of live demo

--- quest-2/code/modules/index.js
+++ quest-2/code/modules/index.js
@@ -32,7 +32,7 @@ app.get('/data', function(req, res) {
 });
 
 // // request data at http://localhost:8080/data or just "/data"
-// app.get('/data', function(req, res) {
+// app.get('/data/last', function(req, res) {
 //   var data = [];  // Array to hold all csv data
 //   var last_row = "";
 //   getLastLine(fileName, 1)
@@ -42,22 +42,22 @@ app.get('/data', function(req, res) {
 //     .catch((err)=> {
 //         console.error(err)
 //     })
-//   fs.createReadStream('../data/sensors.csv')  // path to csv
-//   .pipe(csv())
-//   .on('data', (row) => {
-//     // add thing to check time on row to check if it is a new row. push to data only if it is new
-//     if (row === last_row){
-//       return;
-//     } else {
-//       // console.log(row);
-//       data.push(row);  // Add row of data to array
-//       last_row = row;
-//     }
-//   })
-//   .on('end', () => {
-//     console.log('CSV file successfully processed');
-//     res.send(data);  // Send array of data back to requestor
-//   });
+//   // fs.createReadStream('../data/sensors.csv')  // path to csv
+//   // .pipe(csv())
+//   // .on('data', (row) => {
+//   //   // add thing to check time on row to check if it is a new row. push to data only if it is new
+//   //   if (row === last_row){
+//   //     return;
+//   //   } else {
+//   //     // console.log(row);
+//   //     data.push(row);  // Add row of data to array
+//   //     last_row = row;
+//   //   }
+//   // })
+//   // .on('end', () => {
+//   //   console.log('CSV file successfully processed');
+//   //   res.send(data);  // Send array of data back to requestor
+//   // });
 // });
 
 
