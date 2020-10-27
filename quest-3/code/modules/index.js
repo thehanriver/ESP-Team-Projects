272ec5f1ed01db4da070d604f7369e34cb38cb95
v-cherian <vcherian@bu.edu>
2020-10-26 23:22:01 -0400

    send status with button

--- quest-3/code/modules/index.js
+++ quest-3/code/modules/index.js
@@ -128,7 +128,7 @@ server.on('message', function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
     //num = (num+1)%2;
     // Send Ok acknowledgement
-    $.get('/led-status',function(data,status){
+    $.ajax.get('/led-status',function(data,status){
       led_status = data.led_status;
     })
     server.send(led_status.toString(),remote.port,remote.address,function(error){
