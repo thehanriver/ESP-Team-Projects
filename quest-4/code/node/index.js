cb8e75dd499da0ec5af723be3d78cb78d7214c6e
marioisgay123 <hanmario@bu.edu>
2020-11-13 18:08:24 -0500

    added continuous timer and replace

--- quest-4/code/node/index.js
+++ quest-4/code/node/index.js
@@ -193,14 +193,15 @@ var all ;
 var red;
 var blue;
 var green;
-
+var dateTime;
 //dummy
 ////////////////////////////////////////////////////////////////////////////////////////
+function getTime(){
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-
+dateTime = date+' '+time;
+}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -343,6 +344,7 @@ console.log("clear_flag: ", clear_flag);
 console.log("read_flag: ", read_flag);
 setInterval(function(){checkClear()}, 50);
 setInterval(function(){checkRead()}, 50);
+setInterval(function(){getTime()}, 50);
 app.listen(4000);
 
 
@@ -390,7 +392,7 @@ server.on('message', function (message, remote) {
         break;
     }
     myObj.push({id : id , vote: vote , date_time: dateTime});
-    
+
 
 
     // insert a document into 'customers'
@@ -422,6 +424,8 @@ server.on('message', function (message, remote) {
       assert.equal(null, err);
       console.log("CONNECTED");
       var collection = client.db("Election").collection("Voters");
+      var dbo = db.db("Election");
+      if(collection.find()
       collection.insertMany(myObj, function(err, res) {
         if (err) throw err;
         console.log("1 vote inserted");
@@ -429,8 +433,18 @@ server.on('message', function (message, remote) {
       });
       client.close();
     });
-
-
+    // //Replace example
+    // MongoClient.connect(uri, function(err, db) {
+    //   if (err) throw err;
+    //   var dbo = db.db("mydb");
+    //   var myquery = { address: "Valley 345" };
+    //   var newvalues = { $set: {name: "Mickey", address: "Canyon 123" } };
+    //   dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+    //     if (err) throw err;
+    //     console.log("1 document updated");
+    //     db.close();
+    //   });
+    // });
 
     console.log(remote.address + ':' + remote.port +' - ' + message);
     server.send("vote " + vote.toString() + " from fob " + id.toString() + " recorded",remote.port,remote.address,function(error){
