068321116544a9cdd9e939f3bcda955b43509959
marioisgay123 <hanmario@bu.edu>
2020-11-13 20:43:11 -0500

    Update index.js

--- quest-4/code/node/index.js
+++ quest-4/code/node/index.js
@@ -193,14 +193,14 @@ var all ;
 var red;
 var blue;
 var green;
-
+var dateTime;
 //dummy
 ////////////////////////////////////////////////////////////////////////////////////////
 function getDate(){
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+ dateTime = date+' '+time;
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
